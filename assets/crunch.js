d666693b4c3343cfb3f9f79c243537d8572c220c
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-02-10 23:09:59 +0300

    add nested if else statement

--- assets/crunch.js
+++ assets/crunch.js
@@ -1,8 +1,22 @@
-let message = (login == 'employee') ? 'Hello':
-    (login == 'director') ? 'greetings' :
-    (login == '') ? 'no login':
-    '';
+let  login = prompt('');
+if (login = 'Admin'){
+prompt('password', '');
+let password = '';
+if (password ='TheMaster'){
+    alert('welcome!');
 
-
-
+}
+else if (password = ''){
+    alert('wrong password');
+}
+else {
+    alert('cancelled');
+}
+}
+else if (login = ''){
+    alert('cancelled');
+}
+else {
+    alert('I dont know you');
+}
 
