b83b65bcbb248f6ab99640147c601a4c1f9a51fa
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-02-10 17:01:10 +0300

    create a switch statement

---
+++ assets/crunch.js
@@ -0,0 +1,11 @@
+let browser = '';
+if (browser === 'Edge'){
+    alert("You've got the Edge!");
+}
+
+else if (browser === 'Chrome' || browser === 'Safari' || browser === 'Opera') {
+    alert("okay we support these browsers too");
+
+}
+
+else {"we hope that this page looks ok!"}
