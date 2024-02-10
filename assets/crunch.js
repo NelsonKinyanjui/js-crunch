5f03699edd5e4d12c2de3941105979b9158820f3
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-02-10 17:24:21 +0300

    use prompt in if else

--- assets/crunch.js
+++ assets/crunch.js
@@ -1,15 +1,13 @@
-let a = +prompt('a?','');
-switch (a) {
-case 0:
-    alert(0);
-    break;
-case 1:
-        alert(1);
-        break;
-case 2:
-    case 3:
-    alert('2,3');
-    break;
+let a = prompt();
+if(a > 0) {
+alert(1);
+}
+else if ( a < 0){
+    alert(-1);
 
 }
+else {
+alert(0);
+}
+
 
